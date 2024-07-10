49a0c34ec215663596b331f4c8152dc41ae44103
sayanpal469 <sayanpal469@gmail.com>
2024-07-10 16:42:21 +0530

    service page done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import Nav from "./Nav/Nav";
 import Home from "./Pages/Home";
-import Contact from "./Pages/Contact";
 import About from "./Pages/About";
 import Service from "./Pages/Service";
 
@@ -9,11 +8,10 @@ function App() {
   return (
     <>
       <Nav />
-      <About/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/service" element={<Service/>} />
-        <Route path="/contact" element={<Contact />} />
+        <Route path="/about" element={<About/>} />
       </Routes>
     </>
   );
